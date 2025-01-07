0967b1fa694a172d62c62ceaaf9a0c3ba6ee1881
satishccy <satishchoudhari13579@gmail.com>
2025-01-07 10:03:24 +0530

    prod build

--- projects/votte-frontend/vite.config.ts
+++ projects/votte-frontend/vite.config.ts
@@ -1,6 +1,6 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,4 +12,5 @@ export default defineConfig({
       },
     }),
   ],
-})
+  base: "./",
+});
