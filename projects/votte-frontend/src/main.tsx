2d95816334c9214e37e417dfd122b6295a935183
satishccy <satishchoudhari13579@gmail.com>
2025-01-07 09:57:14 +0530

    initial commit

--- projects/votte-frontend/src/main.tsx
+++ projects/votte-frontend/src/main.tsx
@@ -1,13 +1,41 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './styles/App.css'
-import ErrorBoundary from './components/ErrorBoundary'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./styles/App.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+import { NetworkId, WalletId, WalletManager, WalletProvider } from "@txnlab/use-wallet-react";
+
+const walletManager = new WalletManager({
+  wallets: [
+    WalletId.DEFLY,
+    WalletId.PERA,
+    WalletId.EXODUS,
+    {
+      id: WalletId.LUTE,
+      options: { siteName: "Voting Dapp" },
+    },
+  ],
+  network: NetworkId.TESTNET,
+});
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ErrorBoundary>
+    <WalletProvider manager={walletManager}>
       <App />
-    </ErrorBoundary>
-  </React.StrictMode>,
-)
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
+    </WalletProvider>
+  </React.StrictMode>
+);
